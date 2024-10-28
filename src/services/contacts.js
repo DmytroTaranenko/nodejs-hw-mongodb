2408bac44c5e99868e6ac0c3982f836c164b60b5
Dmytro Taranenko <dmytrotaranenko98@gmail.com>
2024-10-28 16:18:23 +0100

    hw4-validation

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,28 @@
 import { Contact } from '../models/Contact.js';
 
-export const getAllContacts = async () => {
-  return Contact.find();
+export const getAllContacts = async ({ page, perPage, sortBy, sortOrder }) => {
+  const skip = page > 0 ? (page - 1) * perPage : 0;
+
+  const contactQuery = Contact.find();
+
+  const [total, contacts] = await Promise.all([
+    Contact.countDocuments(),
+    contactQuery
+      .sort({ [sortBy]: sortOrder })
+      .skip(skip)
+      .limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(total / perPage);
+
+  return {
+    contacts,
+    page,
+    perPage,
+    totalItems: total,
+    hasNextPage: totalPages - page > 0,
+    hasPreviousPage: page > 1,
+  };
 };
 
 export const getContactById = async (contactId) => {
